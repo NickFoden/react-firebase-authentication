98e1e455a011d88631f0d95ea50a002aff55970c
Nick Foden <nickfoden@gmail.com>
2018-01-17 03:47:43 +0100

    Added firebase index export.

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -7,3 +7,13 @@ export const doCreateUserWithEmailAndPassword = (email, password) =>
 // Sign In
 export const doSignInWithEmailAndPassword = (email, password) =>
   auth.signInWithEmailAndPassword(email, password);
+
+// Sign out
+export const doSignOut = () => auth.signOut();
+
+// Password Reset
+export const doPasswordReset = email => auth.sendPasswordResetEmail(email);
+
+// Password Change
+export const doPasswordUpdate = password =>
+  auth.currentUser.updatePassword(password);
